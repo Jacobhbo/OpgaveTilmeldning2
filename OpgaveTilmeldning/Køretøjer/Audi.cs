5f526d6b6167881addca70128ec91557f0feace7
Tec <Tec@SKAB2-PC7>
2023-08-24 14:53:59 +0100

    init3

--- OpgaveTilmeldning/Køretøjer/Audi.cs
+++ OpgaveTilmeldning/Køretøjer/Audi.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpgaveTilmeldning.Køretøjer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ OpgaveTilmeldning/Køretøjer/Kørtøj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveTilmeldning.Køretøjer
+{
+    internal class Kørtøj
+    {
+        public string? Mærke { get; set; }
+        public string? Model { get; set; }
+
+        public Kørtøj(string? mærke, string? model)
+        {
+            Mærke = mærke; 
+            Model = model;
+        }
+    }
+}
